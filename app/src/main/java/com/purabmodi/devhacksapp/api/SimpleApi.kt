24cf38dcf1698f7af1961074b03eb2712769efcf
Purab Modi <purabmodi2002@gmail.com>
2023-07-10 23:36:17 +0530

    Completed Project for the hackathon

---
+++ app/src/main/java/com/purabmodi/devhacksapp/api/SimpleApi.kt
@@ -0,0 +1,9 @@
+package com.purabmodi.devhacksapp.api
+
+interface SimpleApi {
+
+    // here posts/1 is the end point of the url where the data is present in the website
+//    @POST("/Students/users")
+//    suspend fun sendDetails.(@Field("id")usn :String, @Field("group")group:String): Response<Void>
+// userData is the name of our data class which we created in the first step
+}
